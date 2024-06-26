b42fb7f64229fe40a2e7a82be440be73d850ba1d
Fernando Sávio <fernandosavio86@gmail.com>
2024-06-26 01:09:03 -0300

    Installed and configured vitest

---
+++ src/index.test.ts
@@ -0,0 +1,10 @@
+import { describe, it, assert } from 'vitest'
+import { ultraAdd } from './index'
+
+
+describe('Placeholder test', () => {
+  it('should have a real test', () => {
+    assert.strictEqual(ultraAdd(1, 2), 3);
+  });
+});
+
